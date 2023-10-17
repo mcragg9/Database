37ad8ce078fa9bb188cd6bc8a4d74364e3b13d97
mcragg9 <91230813+mcragg9@users.noreply.github.com>
2023-10-17 16:18:56 -0700

    updated css and some index page layouts

--- __NewDB/index.php
+++ __NewDB/index.php
@@ -47,54 +47,96 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add New Report</title>
+    <title>Report Form</title>
+
 </head>
 <body>
+    <div class="container">
+        <form action="insert_reports.php" method="post">
 
-<h1>Add New Report</h1>
+            <div class="form-group">
+                <label for="incident_date">Incident Date:</label>
+                <input type="date" id="incident_date" name="incident_date" required>
+            </div>
 
-<form action="insert_reports.php" method="post">
+            <div class="form-group">
+                <label for="classification_id">Classification:</label>
+                <select id="classification_id" name="classification_id" required>
+                    <option value="1">Classification 1</option>
+                    <option value="2">Classification 2</option>
+                    <!-- Add more options as needed from your database -->
+                </select>
+            </div>
 
-    <label for="incident_date">Incident Date:</label>
-    <input type="date" id="incident_date" name="incident_date" required><br><br>
+            <div class="form-group">
+                <label for="impact_id">Impact:</label>
+                <select id="impact_id" name="impact_id" required>
+                    <option value="1">Impact 1</option>
+                    <option value="2">Impact 2</option>
+                    <!-- Add more options as needed from your database -->
+                </select>
+            </div>
 
-    <label for="classification_id">Classification:</label>
-    <select id="classification_id" name="classification_id" required>
-        <option value="1">Classification 1</option>
-        <option value="2">Classification 2</option>
-        <!-- Add more options as needed from your database -->
-    </select><br><br>
+            <div class="form-group checkbox-group">
+                <label for="location_id">Location:</label>
+                <select id="location_id" name="location_id" required>
+                    <option value="1">Location 1</option>
+                    <option value="2">Location 2</option>
+                    <!-- Add more options as needed from your database -->
+                </select>
+                <label for="new_location_checkbox">
+                    New Location? 
+                    <input type="checkbox" id="new_location_checkbox">
+                </label>
+                <input type="text" id="new_location" name="new_location" disabled>
+            </div>
 
-    <label for="impact_id">Impact:</label>
-    <select id="impact_id" name="impact_id" required>
-        <option value="1">Impact 1</option>
-        <option value="2">Impact 2</option>
-        <!-- Add more options as needed from your database -->
-    </select><br><br>
+            <div class="form-group">
+                <label for="description">Description:</label><br>
+                <textarea id="description" name="description" rows="4" cols="50" required></textarea>
+            </div>
 
-    <label for="location_id">Location:</label>
-    <select id="location_id" name="location_id" required>
-        <option value="1">Location 1</option>
-        <option value="2">Location 2</option>
-        <!-- Add more options as needed from your database -->
-    </select><br><br>
+            <div class="form-group">
+                <label for="created_by">Created By:</label>
+                <select id="created_by" name="created_by" required>
+                    <?php
+                        $dsn = 'mysql:host=localhost;dbname=tracker';
+                        $username = 'root';
+                        $password = '';
 
-    <label for="description">Description:</label><br>
-    <textarea id="description" name="description" rows="4" cols="50" required></textarea><br><br>
+                        try {
+                            $db = new PDO($dsn, $username, $password);
+                        } catch (PDOException $e) {
+                            echo "Connection failed: " . $e->getMessage();
+                            exit();
+                        }
 
-    <label for="created_by">Created By:</label>
-    <select id="created_by" name="created_by" required>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <!-- Add more options as needed from your database -->
-    </select><br><br>
+                        $stmt = $db->query("SELECT * FROM users");
 
-    <input type="submit" value="Submit">
-    
-</form>
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            echo "<option value=\"{$row['user_id']}\">{$row['FirstName']} {$row['LastName']}</option>";
+                        }
+                    ?>
+                </select>
+            </div>
 
+            <input type="submit" class="submit-button" value="Submit">
+
+        </form>
+    </div>
+
+    <script>
+        document.getElementById('new_location_checkbox').addEventListener('change', function() {
+            var newLocationInput = document.getElementById('new_location');
+            newLocationInput.disabled = !this.checked;
+            if (!this.checked) {
+                newLocationInput.value = '';
+            }
+        });
+    </script>
 </body>
 </html>
 
@@ -102,4 +144,5 @@
 
 
 
-It worked!!!
+</br>
+End of Page
--- __NewDB/insert_reports.php
+++ __NewDB/insert_reports.php
@@ -13,19 +13,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Retrieve values from the form
-    $incident_date = $_POST['incident_date'];
-    $classification_id = $_POST['classification_id'];
-    $impact_id = $_POST['impact_id'];
-    $location_id = $_POST['location_id'];
-    $description = $_POST['description'];
-    $created_by = $_POST['created_by'];
+    $incident_date = $db->quote($_POST['incident_date']);
+    $classification_id = $db->quote($_POST['classification_id']);
+    $impact_id = $db->quote($_POST['impact_id']);
+    $location_id = $db->quote($_POST['location_id']);
+    $description = $db->quote($_POST['description']);
+    $created_by = $db->quote($_POST['created_by']);
 
     // SQL query to insert data into the reports table
     //
     //
     // Remember, my quotes are different? from the class
     $sql = "INSERT INTO reports (IncidentDate, classification_id, impact_id, location_id, Description, CreatedBy, CreatedDate)
-            VALUES ('$incident_date', '$classification_id', '$impact_id', '$location_id', '$description', '$created_by', NOW())"; 
+            VALUES ($incident_date, $classification_id, $impact_id, $location_id, $description, $created_by, NOW())"; 
     
     //Prepare and execute the SQL statement
     $stmt = $db->prepare($sql);
