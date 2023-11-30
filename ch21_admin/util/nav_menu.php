e09e69227cd5056b754a502506f81eaf35af0518
mcragg9 <91230813+mcragg9@users.noreply.github.com>
2023-11-29 17:44:13 -0800

    Final updates again

--- ch21_admin/util/nav_menu.php
+++ ch21_admin/util/nav_menu.php
@@ -1,13 +1,13 @@
-<main>
+<main class="menu">
     <?php
-        if($rights === "Admin") {
-            echo "Administrator Access";
-        } else {
-            echo "Standard User";
-        }
+    if ($rights === "Admin") {
+        echo "Administrator Access";
+    } else {
+        echo "Standard User";
+    }
     ?>
-    <p><a href="index.php?action=show_admin_menu">Main Page</a></p>
-    <p><a href="index.php?action=left_off">Reports</a></p>
-    <p><a href="index.php?action=show_order_manager">Generate Report</a></p>
-    <p><a href="index.php?action=logout">Logout</a></p>
+    <a href="index.php?action=show_admin_menu">Main Page</a>
+    <a href="index.php?action=left_off">Reports</a>
+    <a href="index.php?action=show_order_manager">Generate Report</a>
+    <a href="index.php?action=logout">Logout</a>
 </main>
